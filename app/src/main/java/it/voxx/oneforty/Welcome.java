1e0af4b8a92b06909dbcc4631ee5e9b35ee6901e
fgrotta <fgrotta@byte-code.com>
2015-03-07 12:38:09 +0100

    Add Twitter PlugIn

--- app/src/main/java/it/voxx/oneforty/Welcome.java
+++ app/src/main/java/it/voxx/oneforty/Welcome.java
@@ -4,13 +4,22 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 
 
 public class Welcome extends ActionBarActivity {
 
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "oOBoRQUP5WohyWhduOoWMDPue";
+    private static final String TWITTER_SECRET = "jGcocBTc152xwOkXdsNm4IUdhmaLKCmSuAVAqNcBimjZYSsMCT";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_welcome);
     }
 
